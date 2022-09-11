5bfee8e3536b4cc3be37e4da90361b8c4be12dd6
Alperen <alperenertan98@gmail.com>
2022-09-11 15:44:14 +0300

    minor fix

--- lib/main.dart
+++ lib/main.dart
@@ -3,16 +3,16 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:location/location.dart';
 import 'package:social_eaters/features/login/login_page.dart';
-import 'package:social_eaters/firebase_options.dart';
+//import 'package:social_eaters/firebase_options.dart';
 import 'package:social_eaters/services/auth_service.dart';
 import 'package:social_eaters/services/local_storage.dart';
 import 'bottom_navigation_bar.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  // await Firebase.initializeApp(
+  //   options: DefaultFirebaseOptions.currentPlatform,
+  // );
   await LocalStorage.preferencesInit();
 //
   Location location = Location();
