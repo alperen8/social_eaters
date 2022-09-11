7d3666e51b59ba1115efd4603669ed311c145086
Alperen <alperenertan98@gmail.com>
2022-09-11 15:29:38 +0300

    gitignore update

--- lib/config/constanst.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppConstants {
-  static const Color mainColor = Colors.white;
-  static const apiUrl =
-      "http://ec2-3-72-104-77.eu-central-1.compute.amazonaws.com:8080/api/rest";
-}
